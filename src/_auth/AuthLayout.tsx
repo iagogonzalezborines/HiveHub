3cffd30b45c79abdb1f2ba5dc13747baf9befcb0
iagogonzalezborines <119060993+iagogonzalezborines@users.noreply.github.com>
2024-03-27 12:41:45 +0100

    update

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -12,7 +12,7 @@ function AuthLayout() {
             <Outlet />
           </section>
           <img
-            src="/assets/images/side-img.svg"
+            src="/assets/images/side-img.png"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
